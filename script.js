10817177ad0c9a07832a6439c57a870a1f29c5a9
Kyle Sweigart <kasweigart@gmail.com>
2020-04-07 18:01:47 -0400

    added color scheme

---
+++ script.js
@@ -0,0 +1,33 @@
+const boardSpace = document.querySelectorAll('.boardSpace');
+
+
+const gameBoard = (function(){
+    let gameBoardArr = [];
+
+})();
+
+
+const displayController = (function(){
+
+})();
+
+
+const event = (function(){
+    let triggerSpace = $('#board').on('click', '.boardSpace', function(e) {
+        console.log('nice!');
+    })
+    return {
+        triggerSpace
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+

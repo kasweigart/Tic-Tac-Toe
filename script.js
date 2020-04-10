f9a9f16260fab6abeb0eb118fdcb4a9fb18fde75
Kyle Sweigart <kasweigart@gmail.com>
2020-04-10 12:48:08 -0400

    added heading guide and clear btn functionality

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const boardSpace = document.querySelectorAll('.boardSpace');
+const event = (function () {
+  let gameGuide = document.getElementById("gameGuide");
+  let playersTurn = true;
 
-
-const gameBoard = (function(){
-    let gameBoardArr = [];
-
-})();
-
-
-const displayController = (function(){
-
-})();
-
-
-const event = (function(){
-    let triggerSpace = $('#board').on('click', '.boardSpace', function(e) {
-        console.log('nice!');
-    })
-    return {
-        triggerSpace
+  let triggerSpace = $("#board").on("click", ".boardSpace", function (e) {
+    if (e.target.innerText == "X" || e.target.innerText == "O") {
+      return;
+    } else e.target.innerText == "";
+    switch (playersTurn) {
+      case true:
+        e.target.innerText = "X";
+        playersTurn = false;
+        gameGuide.innerText = "Player Two: Your turn";
+        break;
+      case false:
+        e.target.innerText = "O";
+        playersTurn = true;
+        gameGuide.innerText = "Player One: Your turn";
+        break;
     }
+  });
+
+  let clearBtn = document.querySelector("#clear");
+  let board = document.querySelectorAll(".boardSpace");
+
+  clearBtn.addEventListener("click", function () {
+    [...board].forEach((space) => (space.innerText = ""));
+    playersTurn = true;
+    gameGuide.innerText = "Player One is first. Click any space to start!";
+  });
+
+  return {
+    triggerSpace,
+  };
 })();
 
-
-
-
-
-
-
-
-
-
-
+const deterWinner = (function () {})();
